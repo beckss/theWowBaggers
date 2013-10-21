25d78e5c0ae6065dc95553394a0fa307ba9ad074
Stefan Leutenegger <lestefan@lestefan-MacBookPro.(none)>
2013-10-21 17:42:21 +0200

    added collision_checker with dummy binary collision check

---
+++ tedusar_collision_checker/include/tedusar/check_collision.hpp
@@ -0,0 +1,5 @@
+/// \brief checks for a collision
+/// \parameter x: world-frame x coordinate of the robot base
+/// \parameter y: world-frame y coordinate of the robot base
+/// \parameter theta: yaw angle of the robot base w.r.t. world frame
+bool check_collision(double x, double y, double theta);
